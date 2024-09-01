4d50cbbd44e7e979d29e2b8901db665a5ed7b6a1
drafi0405 <143408086+drafi0405@users.noreply.github.com>
2024-09-02 00:23:00 +0530

    Update Deadlock_detection_Probe_algarithm.c

--- Deadlock_detection_Probe_algarithm.c
+++ Deadlock_detection_Probe_algarithm.c
@@ -1,4 +1,10 @@
-//Probe algarithm
+/*Types of Deadlock detection algarithms
+   1)  WFG - Wait for Graph P1-->P2-->P1, if cycle find Deadlock exists otherwise no
+   2)  Bankers Algarithms, works on safe-state and unsafe state, allocate all required resouce instances to the multiple processes, simulate Allocation resouce algarithm, process them if all processes end with same state no deadlock exists
+   3)  Resource allocation graph  like WFG
+   4)  Probe Algarithm, P1(waiting for resource R2) sends a probe message to P2 to get resource R2 (which is lockaed currently by it), P2 forwards it to P3(which is holding S3) to get S3, if Same message comes back to P1, lock exists
+   5)  Resource allocation matric method
+//simple c Probe algarithm method*/
 #include <stdio.h>
 #include <stdbool.h>
 #define N 3
